5154108213189ef161c92c46bbaf4b3c9b8b1414
Rudolf Jantos <rjantos@users.noreply.github.com>
2025-12-27 15:57:30 +0100

    fixed sitemap integration and site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
-import tailwindcss from '@tailwindcss/vite';
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
